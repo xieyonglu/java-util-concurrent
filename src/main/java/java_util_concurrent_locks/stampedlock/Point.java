958d0b276c31b0802946764dfd1b5b35ce659a6d
谢永路 <xieyonglu@zhongan.com>
2020-01-07 11:43:18 +0800

    代码优化

--- src/main/java/java_util_concurrent_locks/stampedlock/Point.java
+++ src/main/java/java_util_concurrent_locks/stampedlock/Point.java
@@ -17,8 +17,10 @@ import java.util.concurrent.locks.StampedLock;
  *
  */
 public class Point {
+	
 	// 一个点的x，y坐标
 	private double x, y;
+	
 	/**
 	 * Stamped类似一个时间戳的作用，每次写的时候对其+1来改变被操作对象的Stamped值
 	 * 这样其它线程读的时候发现目标对象的Stamped改变，则执行重读
